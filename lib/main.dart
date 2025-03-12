979e202c1906303917c6d7c45d4cd2ea8be76d7c
hyunyoung <youngyoung1036@gmail.com>
2025-03-12 09:18:47 +0900

    로그인 리펙토링

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:movie/auth/login.dart';
-import 'package:movie/providers/auth_provider.dart';
+import 'package:movie/auth/LoginPage.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,45 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Consumer<AuthProvider>(
-        builder: (context, authProvider, child) {
-          return authProvider.isLoggedIn ? const HomeScreen() : const LoginScreen();
-        },
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('홈 화면'),
-        automaticallyImplyLeading: false,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Consumer<AuthProvider>(
-              builder: (context, authProvider, child) {
-                return Text(  // 로그인 상태 표시하기 위한 임시 텍스트
-                  authProvider.isLoggedIn ? '로그인 상태: 로그인' : '로그인 상태: 로그아웃',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          '로그인 성공! 홈 화면입니다.',
-          style: TextStyle(fontSize: 20, color: Colors.black54),
-        ),
-      ),
+      home: const LoginPage(),
     );
   }
 }
