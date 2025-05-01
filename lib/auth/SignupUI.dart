5928e08a4346dfea1ec70e5be9f86b9532adbca8
hyunyoung <118007775+hyunyoung515@users.noreply.github.com>
2025-05-02 02:06:40 +0900

    Update SignupUI.dart
    회원가입 버튼 수정

--- lib/auth/SignupUI.dart
+++ lib/auth/SignupUI.dart
@@ -19,6 +19,7 @@ class SignupUI extends StatelessWidget {
   final VoidCallback onSignUp;
 
   const SignupUI({
+    super.key,
     required this.nameController,
     required this.emailController,
     required this.passwordController,
@@ -37,74 +38,111 @@ class SignupUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _buildInputField(controller: nameController, label: '이름', errorText: nameError),
-        const SizedBox(height: 15),
-        _buildInputField(controller: emailController, label: '이메일', errorText: emailError),
-        const SizedBox(height: 15),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth > 400 ? 400 : constraints.maxWidth * 0.9;
 
-        // 이메일 인증 버튼
-        Row(
-          children: [
-            SizedBox(
-              width: 150,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-                onPressed: onVerificationCode,
-                child: const Text('이메일 인증', style: TextStyle(fontSize: 16, color: Colors.black)),
+        return Center(
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: maxWidth),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _buildInputField(controller: nameController, label: '이름', errorText: nameError),
+                  const SizedBox(height: 15),
+                  _buildInputField(controller: emailController, label: '이메일', errorText: emailError),
+                  const SizedBox(height: 15),
+
+                  // 이메일 인증 버튼
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                            onPressed: onVerificationCode,
+                            child: const Text('이메일 인증', style: TextStyle(fontSize: 16, color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      if (verificationMessage.isNotEmpty)
+                        Expanded(
+                          flex: 5,
+                          child: Text(
+                            verificationMessage,
+                            style: const TextStyle(fontSize: 14, color: Colors.green),
+                          ),
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+
+                  // 이메일 인증 코드 입력
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 6,
+                        child: _buildInputField(
+                          controller: verificationCodeController,
+                          label: '이메일 인증 코드 입력',
+                          errorText: verificationCodeError,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        flex: 3,
+                        child: SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                            onPressed: onVerifyCode,
+                            child: const Text('확인', style: TextStyle(fontSize: 16, color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  _buildInputField(
+                    controller: passwordController,
+                    label: '비밀번호',
+                    errorText: passwordError,
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 15),
+                  _buildInputField(
+                    controller: confirmPasswordController,
+                    label: '비밀번호 확인',
+                    errorText: confirmPasswordError,
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 40),
+
+                  // 회원가입 버튼
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.redAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      onPressed: onSignUp,
+                      child: const Text('회원가입', style: TextStyle(fontSize: 16, color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                ],
               ),
             ),
-            const SizedBox(width: 16),
-            if (verificationMessage.isNotEmpty)
-              Text(verificationMessage, style: const TextStyle(fontSize: 16, color: Colors.green)),
-          ],
-        ),
-        const SizedBox(height: 15),
-
-        // 이메일 인증 코드 입력
-        Row(
-          children: [
-            Expanded(
-              child: _buildInputField(
-                  controller: verificationCodeController, label: '이메일 인증 코드 입력', errorText: verificationCodeError),
-            ),
-            const SizedBox(width: 16),
-            SizedBox(
-              width: 100,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-                onPressed: onVerifyCode,
-                child: const Text('확인', style: TextStyle(fontSize: 16, color: Colors.black)),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 15),
-        _buildInputField(controller: passwordController, label: '비밀번호', errorText: passwordError, obscureText: true),
-        const SizedBox(height: 15),
-        _buildInputField(controller: confirmPasswordController, label: '비밀번호 확인', errorText: confirmPasswordError, obscureText: true),
-        const SizedBox(height: 50),
-
-        // 회원가입 버튼
-        SizedBox(
-          width: double.infinity,
-          height: 50,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.redAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-            ),
-            onPressed: onSignUp,
-            child: const Text('회원가입', style: TextStyle(fontSize: 16, color: Colors.white)),
           ),
-        ),
-        const SizedBox(height: 50),
-      ],
+        );
+      },
     );
   }
 
