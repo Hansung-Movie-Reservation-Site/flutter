15ee2c7f4769f75e43d0429895ae1d485e0e82f9
ghkdwnsdlr <imbanana15@gmail.com>
2025-05-15 01:07:20 +0900

    추천 기능 성공

--- lib/Recommend/recommendmovie.dart
+++ lib/Recommend/recommendmovie.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../Common/movie.dart';
 import '../Common/navbar.dart';
 import '../Common/SearchModal.dart';
 import 'package:http/http.dart' as http;
 
+import '../providers/auth_provider.dart';
 import '../reserve/TheaterPage.dart';
 
 class RecommendMovie extends StatefulWidget {
@@ -22,12 +25,23 @@ class _ProductListPageState extends State<RecommendMovie> {
   Map<String, dynamic> result = {}; // 초기값을 빈 Map으로 설정
   int movie_id = 0; // 기본값 0으로 설정
   var reason ="";
+  int? user_id = -1;
+  String username = "알 수 없음";
+  Movie? movie_detail = null;
 
   String searchKeyword = ''; // 검색어 상태
   List<Movie> products = [];
 
   sendPost() async {
-    var url = Uri.parse('http://localhost:8080/api/v1/AIRecommand/synopsis');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getKeys().forEach((key) {
+      print('$key: ${prefs.get(key)} (${prefs.get(key)?.runtimeType})');
+    });
+
+    user_id = prefs.getInt("user_id");
+    username = prefs.getString("username")?? username;
+
+    var url = Uri.parse('https://hs-cinemagix.duckdns.org/api/v1/AIRecommand/synopsis');
 
     var response = await http.post(
       url,
@@ -35,7 +49,7 @@ class _ProductListPageState extends State<RecommendMovie> {
         'Content-Type': 'application/json;charset=UTF-8', // JSON 전송 시
       },
       body: jsonEncode({
-        "user_id": 9
+        "user_id": user_id
       }),
     );
 
@@ -53,10 +67,38 @@ class _ProductListPageState extends State<RecommendMovie> {
     }
   }
 
+  findMovie() async {
+    final response = await http.get(Uri.parse('https://hs-cinemagix.duckdns.org/api/v1/movies/searchById?id=${movie_id}'));
+    ///search/id
+    if (response.statusCode == 200) {
+      final utf8Body = utf8.decode(response.bodyBytes);
+      Movie data = Movie.fromJson(json.decode(utf8Body));
+      //final fetched = data.map((json) => Movie.fromJson(json));
+      movie_detail = data;
+    } else {
+      print('에러 코드: ${response.statusCode}');
+    }
+  }
   @override
   void initState() {
     super.initState();
-    sendPost();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await context.read<MovieStore>().getMovieData(); // 영화 데이터 먼저 로딩
+      await sendPost(); // movie_id 값 받아오기
+
+      // movie_id가 설정된 후 findMovie 실행
+      print("movie_id: "+movie_id.toString());
+      await findMovie();
+
+      if (movie_detail != null) {
+        print("movie_detail: ${movie_detail!.id}");
+      } else {
+        print("해당 ID의 영화를 찾을 수 없습니다.");
+      }
+
+      setState(() {}); // 상태 업데이트
+    });
   }
 
   @override
@@ -97,7 +139,7 @@ class _ProductListPageState extends State<RecommendMovie> {
       body: ListView(
         padding: const EdgeInsets.all(12),
         children: [
-          if (result.isNotEmpty)Padding(
+          if (result.isNotEmpty && movie_detail != null)Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Container(
                 padding: const EdgeInsets.all(12),
@@ -114,7 +156,7 @@ class _ProductListPageState extends State<RecommendMovie> {
                         ClipRRect(
                           //borderRadius: BorderRadius.circular(12),
                           child: Image.network(
-                            'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000089/89058/89058_320.jpg',
+                            movie_detail?.posterImage ?? '',
                             width: MediaQuery.of(context).size.width * 0.8,  // 화면 크기의 80%로 설정
                             height: MediaQuery.of(context).size.height * 0.4, // 화면 크기의 40%로 설정
                             fit: BoxFit.contain,
@@ -126,7 +168,7 @@ class _ProductListPageState extends State<RecommendMovie> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("추천 영화: "+movie_id.toString()),
+                        Text(username+"님의 추천 영화: "+movie_detail!.title),
                         Text("추천 이유: "+reason),
                         Align(
                             alignment: Alignment.center,
@@ -143,32 +185,7 @@ class _ProductListPageState extends State<RecommendMovie> {
                                   ),
                                 ),child: Text("예매하기"))
                         )
-                      ],))),
-                    SizedBox(width: 10,height: 10),
-                    AppContainer(child:
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Summary',
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 8),
-                          const Text(
-                            '미키 17 (15세)',
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 8),
-                          const Text('2시간 17분', style: TextStyle(fontSize: 16)),
-                          const SizedBox(height: 4),
-                          const Text('감독: 봉준호', style: TextStyle(fontSize: 16)),
-                          const Text('장르: 어드벤처, SF, 드라마', style: TextStyle(fontSize: 16)),
-                          const Text('개봉: 2025.02.28', style: TextStyle(fontSize: 16)),
-                          const SizedBox(height: 10),
-
-                          // 예매하기 버튼
-                        ],),),)],)),)// movie_id 출력
+                      ],)))],)),)
           else
             Center(child: Text('로딩중')),
         ],
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../Common/movie.dart';
 
 class AuthProvider with ChangeNotifier {
   bool _isLoggedIn = false;
@@ -15,3 +20,41 @@ class AuthProvider with ChangeNotifier {
     notifyListeners(); // UI 업데이트
   }
 }
+
+
+class MovieStore with ChangeNotifier {
+  var name = 'jun';
+  var movies = [];
+
+  Movie? findMovie(int i){
+    print("호출1");
+    for(Movie m in movies){
+      print("id: "+m.id.toString());
+      if(m.id == i)return m;
+    }
+    print("호출2");
+    return null;
+  }
+
+  Future<void> getMovieData() async {
+    final response = await http.get(Uri.parse('https://hs-cinemagix.duckdns.org/api/v1/movies/daily'));
+    ///search/id
+    if (response.statusCode == 200) {
+      final utf8Body = utf8.decode(response.bodyBytes);
+      List<dynamic> data = json.decode(utf8Body);
+      final fetched = data.map((json) => Movie.fromJson(json)).toList();
+      for (var movie in fetched) {
+        print('getMovieData - 제목: ${movie.title}, 이미지: ${movie.posterImage}');
+        movies.add(movie);
+      }
+    } else {
+      print('에러 코드: ${response.statusCode}');
+    }
+  }
+
+  changeName(){
+    name = 'john';
+    notifyListeners();
+  }
+}
+
