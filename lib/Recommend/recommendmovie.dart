3b6dc847103f7d4e02524af9f6d6a911037e24c5
ghkdwnsdlr <imbanana15@gmail.com>
2025-05-15 21:01:44 +0900

    추천 기능 디자인4 및 새로고침 기능 추가

--- lib/Recommend/recommendmovie.dart
+++ lib/Recommend/recommendmovie.dart
@@ -30,9 +30,12 @@ class _ProductListPageState extends State<RecommendMovie> {
   String username = "알 수 없음";
   Movie? movie_detail = null;
   bool _isHovering = false;
+  String movie_poster = "";
 
   String searchKeyword = ''; // 검색어 상태
   List<Movie> products = [];
+
+  bool isLoading = false;
 
   sendPost() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -66,6 +69,7 @@ class _ProductListPageState extends State<RecommendMovie> {
       print(result);
     } else {
       print('오류 발생: ${response.statusCode}');
+      showErrorDialog(context, '오류 발생: ${response.statusCode}');
     }
   }
 
@@ -79,12 +83,13 @@ class _ProductListPageState extends State<RecommendMovie> {
       movie_detail = data;
     } else {
       print('에러 코드: ${response.statusCode}');
+      showErrorDialog(context, '에러 코드: ${response.statusCode}');
     }
   }
   @override
   void initState() {
     super.initState();
-
+    setState(() {isLoading = true;});
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await context.read<MovieStore>().getMovieData(); // 영화 데이터 먼저 로딩
       if(movie_id != 0) return;
@@ -100,7 +105,7 @@ class _ProductListPageState extends State<RecommendMovie> {
         print("해당 ID의 영화를 찾을 수 없습니다.");
       }
 
-      setState(() {}); // 상태 업데이트
+      setState(() {movie_poster = movie_detail!.posterImage; isLoading = false;}); // 상태 업데이트
     });
   }
 
@@ -140,7 +145,7 @@ class _ProductListPageState extends State<RecommendMovie> {
           )
         ],
       ),
-      body: result.isNotEmpty && movie_detail != null
+      body: isLoading == false
           ? Container(
         child: SingleChildScrollView(
             child: Stack(
@@ -149,9 +154,7 @@ class _ProductListPageState extends State<RecommendMovie> {
           clipBehavior: Clip.none, // ← overflow 허용
           children: [
             Positioned(
-              top: 0,
-              left: 0,
-              right: 0,      // 좌우 꽉 채우기
+              top: 0, left: 0, right: 0,      // 좌우 꽉 채우기
               height: 150,   // 높이 150
               child: Container(
                 color: Colors.redAccent,
@@ -164,30 +167,25 @@ class _ProductListPageState extends State<RecommendMovie> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(margin: EdgeInsets.only(left: 30),
-                      child: Row(children: [ Image.asset('AI.png', width: 50, height: 50, color: Colors.white,),Text("  AI 추천", style: TextStyle(fontSize: 29,color: Colors.white))],)),
+                  Container(margin: EdgeInsets.only(left: 30, top: 15),
+                      child: Row(children: [
+                        Image.asset('AI.png', width: 50, height: 50, color: Colors.white,),
+                        SizedBox(width: 20),
+                        Text(username+"님을 위한 AI 추천", style: TextStyle(fontSize: 27,color: Colors.white)), Spacer(),
+                        IconButton(onPressed: () async {
+                          setState(() {isLoading = true;});
+                          await sendPost();
+                          await findMovie();
+                          setState(() {movie_poster = movie_detail!.posterImage; isLoading = false;});
+                          }, icon: Image.asset("reload.png", height: 20, width: 20, color: Colors.white)),],)),
                   SizedBox(height: 5,),
                   Container(
                     padding: EdgeInsets.all(5),
                     //decoration: Border,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2), // 그림자 색상
-                          offset: Offset(0, 4), // x=0, y=4 → 아래로 그림자
-                          blurRadius: 6,        // 퍼짐 정도
-                          spreadRadius: 0,      // 확장 정도
-                        ),
-                      ],
-                    ),
+                    decoration: commonBoxDecoration(top: true),
                     child: ClipRRect(
                       child: Image.network(
-                        movie_detail?.posterImage ?? '',
+                        movie_poster,
                         width: MediaQuery.of(context).size.width * 0.6,
                         height: MediaQuery.of(context).size.height * 0.58,
                         fit: BoxFit.contain,
@@ -197,17 +195,7 @@ class _ProductListPageState extends State<RecommendMovie> {
                   Container(
                       padding: EdgeInsets.all(5),
                       //decoration: Border,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2), // 그림자 색상
-                            offset: Offset(0, 4), // x=0, y=4 → 아래로 그림자
-                            blurRadius: 6,        // 퍼짐 정도
-                            spreadRadius: 0,      // 확장 정도
-                          ),
-                        ],
-                      ),
+                      decoration: commonBoxDecoration(),
                       width: MediaQuery.of(context).size.width * 0.62,
                       child: Text(
                         movie_detail!.title,
@@ -217,28 +205,14 @@ class _ProductListPageState extends State<RecommendMovie> {
                   Container(
                       padding: EdgeInsets.all(5),
                       //decoration: Border,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(5),
-                          bottomRight: Radius.circular(5),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2), // 그림자 색상
-                            offset: Offset(0, 4), // x=0, y=4 → 아래로 그림자
-                            blurRadius: 6,        // 퍼짐 정도
-                            spreadRadius: 0,      // 확장 정도
-                          ),
-                        ],
-                      ),
+                      decoration: commonBoxDecoration(bottom: true),
                       width: MediaQuery.of(context).size.width * 0.62,
                       child:
                   Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         const SizedBox(height: 2),
-                        Text("추천 이유: $reason"),
+                        Text(" 추천 이유: $reason"),
                         const SizedBox(height: 5),
                         MouseRegion(
                           onEnter: (_) => setState(() => _isHovering = true),
@@ -252,13 +226,15 @@ class _ProductListPageState extends State<RecommendMovie> {
                                 ),
                               );
                             },
-                            child: Text(
+                            child: Align(alignment: Alignment.topLeft,
+                                child: Text(
                               "더 자세히...",
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 fontSize: 15,
-                                color: _isHovering ? Colors.red : Colors.white,
+                                color: _isHovering ? Colors.red : Colors.black,
                               ),
+                            )
                             ),
                           ),
                         ),
@@ -304,32 +280,70 @@ class _ProductListPageState extends State<RecommendMovie> {
   }
 }
 
-
-class AppContainer extends StatelessWidget {
-  final Widget child;
-  final EdgeInsetsGeometry? padding;
-  final EdgeInsetsGeometry? margin;
-
-  const AppContainer({
-    super.key,
-    required this.child,
-    this.padding,
-    this.margin,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity, // ✅ 부모 너비에 맞춰 확장
-      margin: margin,
-      padding: padding ?? const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white70,
-        borderRadius: BorderRadius.circular(5),
+BoxDecoration commonBoxDecoration({bool top = false, bool bottom = false}) {
+  return BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(top ? 5 : 0),
+      topRight: Radius.circular(top ? 5 : 0),
+      bottomLeft: Radius.circular(bottom ? 5 : 0),
+      bottomRight: Radius.circular(bottom ? 5 : 0),
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.2),
+        offset: Offset(0, 4),
+        blurRadius: 6,
+        spreadRadius: 0,
       ),
-      child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: child),
-    );
-  }
+    ],
+  );
 }
+void showErrorDialog(BuildContext context, String status) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('에러 발생'),
+        content: Text("status"),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('확인'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+// class AppContainer extends StatelessWidget {
+//   final Widget child;
+//   final EdgeInsetsGeometry? padding;
+//   final EdgeInsetsGeometry? margin;
+//
+//   const AppContainer({
+//     super.key,
+//     required this.child,
+//     this.padding,
+//     this.margin,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: double.infinity, // ✅ 부모 너비에 맞춰 확장
+//       margin: margin,
+//       padding: padding ?? const EdgeInsets.all(12),
+//       decoration: BoxDecoration(
+//         color: Colors.white70,
+//         borderRadius: BorderRadius.circular(5),
+//       ),
+//       child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 16),
+//           child: child),
+//     );
+//   }
+// }
